8aae98a533f093b8e95cd3b719e8cade43d9fc7c
MohamedIbrahim <mohamed.ibrhim6553@gmail.com>
2023-02-10 22:25:06 +0200

    " scripts_zoom"

---
+++ Scripts/zoom/zoom.py
@@ -0,0 +1,88 @@
+import os
+import time
+import datetime
+import urllib.request
+import pyautogui as py
+from selenium import webdriver
+
+
+def connect(host='http://google.com'):
+    try:
+        urllib.request.urlopen(host) #Python 3.x
+        return True
+    except:
+        return False
+
+
+def picCoordinates(picture):
+    r= None
+    while r is None:
+        r=py.locateCenterOnScreen(picture,grayscale=False,confidence=.7)
+    return r.x,r.y
+
+
+def join(meet_code,passcode):
+    driver = webdriver.Chrome()
+    driver.get('https://zoom.us/join')
+    
+    # --> storing meeting id and passcode, you may also take this as an input in your code from the user
+    
+    meet_code1 = meet_code[:3]
+    meet_code2 = meet_code[3:6]
+    meet_code3 = meet_code[6:]
+
+    element_box=driver.find_element("xpath","//input[@id='join-confno']")
+    time.sleep(1) 
+    element_box.send_keys(meet_code1)
+    time.sleep(0.5) 
+    element_box.send_keys(meet_code2)
+    time.sleep(0.5) 
+    element_box.send_keys(meet_code3) 
+    #waiting for 2 seconds to send the code
+    time.sleep(2) 
+    #finding the join button and clicking on it
+    
+    Btn = driver.find_element("xpath","//a[@id='btnSubmit']")
+    Btn.click()
+    
+    X,Y =picCoordinates("openZoomMeetings.jpeg")
+    py.moveTo(clicks=2,x=X, y=Y)
+    time.sleep(0.5) 
+    py.click(clicks=2,x=X, y=Y)
+    
+    X,Y =picCoordinates("passcode.jpeg")
+    py.click(clicks=2,x=X, y=Y)
+    time.sleep(1) 
+
+    py.typewrite(passcode,interval=0.25)
+
+    X,Y =picCoordinates("joinMeeting.jpg")
+    py.click(clicks=2,x=X, y=Y)
+    
+    if picCoordinates("mute.jpg"):
+        py.hotkey('alt', 'a')
+
+if __name__ == "__main__":
+    meet_code="6224511075"
+    passcode = "IMT-S6J9T2"
+    x = datetime.datetime.now()
+    days=["Monday","Tuesday","Wednesday","Thursday"]
+    while True:
+        if connect(): 
+            if x.strftime("%A") in days:
+                if x.strftime("%H")==20:
+                    join(meet_code,passcode)
+            break
+        else:
+            # input Wifi name
+            time.sleep(2)
+            name_of_router = "Mohammad"
+            # connect to the given wifi network
+            os.system(f'''cmd /c "netsh wlan connect name={name_of_router}"''')
+
+
+        
+    
+    
+    
+    

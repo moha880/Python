ef87430291748a8504124ce6b162ccb974a3c9b1
MohamedIbrahim <mohamed.ibrhim6553@gmail.com>
2023-02-15 20:28:10 +0200

    "Valentine"

--- Lectures/Lecture4/Lec4.py
+++ Lectures/Lecture4/Lec4.py
@@ -1,3 +1,41 @@
-x=[range(100)]
-print(x)        
- 
+# pip freeze | grep pyfiglet
+##############################################
+
+# Import the required module for text
+# # to speech conversion
+# from gtts import gTTS
+
+# # This module is imported so that we can
+# # play the converted audio
+# import os
+
+# # The text that you want to convert to audio
+# mytext = 'Welcome to geeksforgeeks!'
+
+# # Language in which you want to convert
+# language = 'en'
+
+# # Passing the text and language to the engine,
+# # here we have marked slow=False. Which tells
+# # the module that the converted audio should
+# # have a high speed
+# myobj = gTTS(text=mytext, lang=language, slow=False)
+
+# # Saving the converted audio in a mp3 file named
+# # welcome
+# myobj.save("welcome.mp3")
+
+# # Playing the converted file
+# os.system("mpg321 welcome.mp3")
+
+##############################################
+
+# [print(i) for i in range(10) if i %2==0]
+
+##############################################
+
+import pyfiglet
+
+str=pyfiglet.figlet_format("mohamed")
+
+print(str)
